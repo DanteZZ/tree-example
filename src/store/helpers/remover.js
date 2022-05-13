358c1286a357a2a04ca80597a06c9fe02c0463aa
Trofimov Maxim <dantezzstudio@gmail.com>
2022-05-13 23:11:02 +0700

    fix: Отрефакторил фундкцию удаления

--- src/store/helpers/remover.js
+++ src/store/helpers/remover.js
@@ -2,17 +2,10 @@ export const remover = (tree, id, all = false) => {
   // Возвращает массив из удаляемого id и дочерних id
   const ids = [];
   tree.forEach((i) => {
-    if (all) {
+    if (i.id == id || all) {
       ids.push(i.id);
-      ids.push(remover(i.childrens, id, all));
-    } else {
-      if (i.id == id) {
-        ids.push(i.id);
-        ids.push(remover(i.childrens, id, true));
-      } else {
-        ids.push(remover(i.childrens, id, all));
-      }
     }
+    ids.push(remover(i.childrens, id, i.id == id ? true : all));
   });
   return ids.flat();
 };
